a480f664e25b3f246b18bba8e4e0a78f2ef45088
AngeTia <tiaange03@gmail.com>
2023-01-24 19:24:59 +0000

    Update Daily

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'daily',
+  appName: 'DailyChallenge',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://swapi.dev/api/films").subscribe(
+      films => console.log(films)
+    );
+  }
 }
